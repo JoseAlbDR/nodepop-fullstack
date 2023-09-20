6e41fa7518ab1a78ddb706360e2d81aca3e36bec
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-20 12:08:28 +0200

    feat: test server

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log('Server is working');
+import express from 'express';
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.send('Hello there');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
