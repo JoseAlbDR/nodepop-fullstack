aacd077a6c56edbf08bfc745973c42195b789854
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-20 23:25:12 +0200

    fix: types in randomTags

--- src/utils/randomTags.ts
+++ src/utils/randomTags.ts
@@ -1,6 +1,8 @@
-export const getRandomTags = (n: number) => {
-  const tags = ['work', 'lifestyle', 'motor', 'mobile'];
-  const randomTags: string[] = [];
+import { ITags } from '../types/productInterfaces';
+
+export const getRandomTags = (n: number): ITags[] => {
+  const tags: ITags[] = ['work', 'lifestyle', 'motor', 'mobile'];
+  const randomTags: ITags[] = [];
   while (randomTags.length < n) {
     const randomPos = Math.floor(Math.random() * tags.length);
     if (!randomTags.includes(tags[randomPos])) {
