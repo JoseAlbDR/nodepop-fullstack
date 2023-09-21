bf898d8dddf17de375cf5ffd8c0cc20f529bd5ff
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-21 19:41:31 +0200

    feat: debug server

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import 'dotenv/config';
 import morgan from 'morgan';
 import express from 'express';
 import fileUpload from 'express-fileupload';
+import debug from 'debug';
+const serverDebug = debug('nodepop-ts:server');
 
 // DB
 import { dbConnect } from './db/dbConnect';
@@ -38,7 +40,7 @@ const start = async () => {
     await dbConnect(process.env.MONGO_URL);
     console.log('Connected to db');
     app.listen(port, () => {
-      console.log(`Server listening on port: ${port}`);
+      serverDebug(`Server listening on port: ${port}`);
     });
   } catch (error) {
     console.log(error);
