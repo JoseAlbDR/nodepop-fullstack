e352c7beb672e8dcb28908055e60c815d5b8b937
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-21 16:49:54 +0200

    fix: delete notFoundMIddleware

--- src/middleware/notFoundMIddleware.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
-
-const notFoundMiddleware = (req: Request, res: Response) => {
-  res.status(StatusCodes.NOT_FOUND).send(`Route ${req.url} not found`);
-};
-
-export default notFoundMiddleware;
