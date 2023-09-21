c58d7908dab5d52b058096babc754f02bd0268fa
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-21 17:14:09 +0200

    feat: User model

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,22 +1,25 @@
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  lastName: {
-    type: String,
-    default: 'lastName',
+const UserSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+    lastName: {
+      type: String,
+      default: 'lastName',
+    },
+    location: {
+      type: String,
+      default: 'my city',
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
   },
-  location: {
-    type: String,
-    default: 'my city',
-  },
-  role: {
-    type: String,
-    enum: ['user', 'admin'],
-    default: 'user',
-  },
-});
+  { timestamps: true }
+);
 
 export const User = mongoose.model('User', UserSchema);
