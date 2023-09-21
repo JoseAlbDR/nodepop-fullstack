eea6d97dee79162b21f1669b8ffd54eb08d0e66e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-21 19:13:08 +0200

    feat: use miidleware validateRegisterUser in register route

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { authController } from '../controllers/authController';
+import { validateRegisterUser } from '../middleware/validationMiddleware';
 
 const router = express.Router();
 
 router.post('/login', authController.login);
-router.post('/register', authController.register);
+router.post('/register', validateRegisterUser, authController.register);
 
 export default router;
