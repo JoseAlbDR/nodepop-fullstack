4f936e02f63c3f247d6306b1b1e8fdc38166ffae
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-21 10:49:01 +0200

    feat: use validation middleware to validate createProduct controller

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import productController from '../controllers/productController';
 import uploadsController from '../controllers/uploadsController';
+import { validateProductCreation } from '../middleware/validation';
 
 const router = express.Router();
 
 router
   .route('/')
   .get(productController.getAllProducts)
-  .post(productController.createProduct);
+  .post(validateProductCreation, productController.createProduct);
 
 router.get('/tags', productController.getAllTags);
 router.post('/uploadImage', uploadsController.uploadProductImage);
