b132dea31642e02b22ab798c9864fd1fc7adc163
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-21 17:13:23 +0200

    feat: IUser interface

--- src/types/authInterfaces.ts
+++ src/types/authInterfaces.ts
@@ -1,1 +1,28 @@
 export type Role = 'admin' | 'user';
+
+export interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  lastName: string;
+  location: string;
+  role: Role;
+}
+
+// name: String,
+//   email: String,
+//   password: String,
+//   lastName: {
+//     type: String,
+//     default: 'lastName',
+//   },
+//   location: {
+//     type: String,
+//     default: 'my city',
+//   },
+//   role: {
+//     type: String,
+//     enum: ['user', 'admin'],
+//     default: 'user',
+//   },
+// });
