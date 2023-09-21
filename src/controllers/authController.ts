66b1fb2f3cceb996d143e3334e07ebc826f2fb36
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-21 17:46:49 +0200

    feat: register controller and service initial logic

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from 'express';
 import { authService } from '../services/authService';
+import { RegisterUserDTO } from '../dto/registerUserDto';
+import { StatusCodes } from 'http-status-codes';
 
 export const authController = {
-  register: async (_req: Request, res: Response) => {
-    res.send(authService.register());
+  register: async (req: RegisterUserDTO, res: Response) => {
+    const user = await authService.register(req.body);
+
+    res.status(StatusCodes.CREATED).json({ msg: 'user created', user });
   },
 
   login: async (_req: Request, res: Response) => {
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,9 +1,20 @@
+import { User } from '../models/UserModel';
+import { IUser } from '../types/authInterfaces';
+
 export const authService = {
-  register: () => {
-    return 'register';
+  register: async (user: IUser) => {
+    const isFirstAccount = (await User.countDocuments({})) === 0;
+
+    const role = isFirstAccount ? 'admin' : 'user';
+
+    user.role = role;
+
+    const response = await User.create(user);
+
+    return response;
   },
 
-  login: () => {
+  login: async () => {
     return 'login';
   },
 };
