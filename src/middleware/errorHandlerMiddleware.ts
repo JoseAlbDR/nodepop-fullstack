b1f9c4caaef17e28a08166044909f254570ed009
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-22 00:33:37 +0200

    feat: set email to unique and check for duplicate email error in MongoDB

--- src/middleware/errorHandlerMiddleware.ts
+++ src/middleware/errorHandlerMiddleware.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 import { CustomAPIError } from '../errors/customError';
 import { StatusCodes } from 'http-status-codes';
+import { MongoError } from 'mongodb';
+
+interface IDuplicateMongoError extends MongoError {
+  keyValue: {
+    [x: string]: string;
+  };
+}
 
 const errorHandlerMiddleware = (
   err: Error,
@@ -10,15 +17,28 @@ const errorHandlerMiddleware = (
 ) => {
   console.log(err);
 
-  const statusCode =
-    err instanceof CustomAPIError
-      ? err.statusCode
-      : StatusCodes.INTERNAL_SERVER_ERROR;
+  let msg, statusCode;
 
-  const msg =
-    'message' in err ? err.message : 'Internal Server Error Try Again Later';
+  // Custom errors
+  if (err instanceof CustomAPIError) {
+    statusCode = err.statusCode;
+    msg = err.message;
+  }
 
-  return res.status(statusCode).json({ name: err.name, msg, stack: err.stack });
+  // Duplicate UNIQUE email error
+  if (err instanceof MongoError && err.code === 11000) {
+    statusCode = StatusCodes.CONFLICT;
+    msg = `Duplicate value entered for ${Object.keys(
+      (err as IDuplicateMongoError).keyValue
+    ).join(', ')} please enter a valid value`;
+  }
+
+  // Check rest of errors
+  return res.status(statusCode || StatusCodes.INTERNAL_SERVER_ERROR).json({
+    name: err.name,
+    msg: msg || 'Internal Server Error Try Again Later',
+    stack: err.stack,
+  });
 };
 
 export default errorHandlerMiddleware;
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema(
   {
     name: String,
-    email: String,
+    email: { type: String, unique: true },
     password: String,
     lastName: {
       type: String,
