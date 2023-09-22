0d8ee2e6cd900a8e986ccccc34e2bb5d23af735e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-23 01:08:42 +0200

    feat: setup router in vite config and test route in main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+fetch('http://localhost:3000/api/v1/test')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
