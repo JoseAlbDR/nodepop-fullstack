8874d8dc761ea22e6a851291ba8c7a251bd6ae06
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-23 01:34:31 +0200

    style: remove test fetch

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import customFetch from './utils/customFetch.ts';
-
-const { data } = await customFetch.get('/test');
-console.log(data);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
