79cf5ad44fb955f503b0694d6e6dba300097e5b8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-22 17:24:21 +0200

    refactor: change requestUser type to JWTPayload

--- src/utils/checkPermissionsUtil.ts
+++ src/utils/checkPermissionsUtil.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-import { ITokenPayload } from '../types/authInterfaces';
+import { JWTPayload } from '../types/authInterfaces';
 import { UnauthorizedError } from '../errors';
 
 export const checkPermissions = (
-  requestUser: ITokenPayload,
+  requestUser: JWTPayload,
   resourceUserId: mongoose.Types.ObjectId
 ) => {
   if (requestUser.role === 'admin') return;
