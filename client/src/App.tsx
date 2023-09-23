d216b0ef549e92b51504ae8121559eaa3689505d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-23 13:01:07 +0200

    feat: add loader to dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ import {
 } from './pages';
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
-
+import { loader as dashboardLoader } from './pages/DashboardLayout';
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
   document.body.classList.toggle('dark-theme', isDarkTheme);
@@ -46,6 +46,7 @@ const router = createBrowserRouter([
       {
         path: 'dashboard',
         element: <DashboardLayout />,
+        loader: dashboardLoader,
         children: [
           {
             index: true,
