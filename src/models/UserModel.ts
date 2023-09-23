7f3972506ada5fea3722e92ddda2e819909f6542
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-23 10:49:15 +0200

    feat: add index to email to by unique

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -5,7 +5,7 @@ import { hashPassword } from '../utils/hashPasswordUtil';
 const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, index: true },
     password: { type: String, required: [true, 'password is required'] },
     lastName: {
       type: String,
