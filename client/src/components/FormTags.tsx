3d6a91e64d14177af5fe4aa3ea642e933dd19504
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-23 19:19:08 +0200

    feat: new component FormTags

---
+++ client/src/components/FormTags.tsx
@@ -0,0 +1,28 @@
+import { ITags } from '../../../src/types/productInterfaces';
+import { useTags } from '../hooks/useTags';
+
+const FormTags = () => {
+  const { data, isLoading: isLoadingTags } = useTags();
+  if (isLoadingTags) return;
+  return (
+    <div className="form-row">
+      <label className="form-label">tags</label>
+      <fieldset className="form-tags">
+        {data.tags.map((tag: ITags, index: number) => (
+          <label key={tag}>
+            <input
+              type="checkbox"
+              name="tags"
+              value={tag}
+              className="input-check"
+              defaultChecked={index === 0}
+            />
+            {tag}
+          </label>
+        ))}
+      </fieldset>
+    </div>
+  );
+};
+
+export default FormTags;
