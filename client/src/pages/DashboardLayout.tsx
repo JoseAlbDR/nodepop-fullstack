6b20f9122f96e2ecfde180b9495a017e10306656
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-23 13:02:20 +0200

    feat: loader to fetch user data from /users/current-user path

--- client/src/pages/DashboardLayout.tsx
+++ client/src/pages/DashboardLayout.tsx
@@ -1,8 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, redirect } from 'react-router-dom';
 import StyledDashboard from '../assets/wrappers/Dashboard';
 import { BigSidebar, SmallSideBar } from '../components';
 import NavBar from '../components/NavBar';
 import { DashboardProvider } from '../context/DashboardContext';
+import customFetch from '../utils/customFetch';
+import { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
+
+export const loader = async () => {
+  try {
+    const { data } = await customFetch('/users/current-user');
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      toast.error(error?.response?.data?.msg);
+    }
+    return redirect('/');
+  }
+};
 
 const DashboardLayout = () => {
   return (
