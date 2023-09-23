5ca57380805209beabecef362bef1419e2a0a067
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-23 19:37:05 +0200

    feat: add FormTags and FormSelect to components index

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -3,3 +3,5 @@ export { default as FormRow } from './FormRow';
 export { default as NavBar } from './NavBar';
 export { default as BigSidebar } from './BigSidebar';
 export { default as SmallSideBar } from './SmallSidebar';
+export { default as FormSelect } from './FormSelect';
+export { default as FormTags } from './FormTags';
