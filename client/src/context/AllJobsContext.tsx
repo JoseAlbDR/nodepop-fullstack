edd47a4e5f6aaa49586278313d9c985a9d72c980
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-24 12:00:59 +0200

    feat: AllJobsContext

---
+++ client/src/context/AllJobsContext.tsx
@@ -0,0 +1,38 @@
+import React, { createContext, useContext } from 'react';
+import { IProduct, IProductResponse } from '../types/Products';
+import { useLoaderData } from 'react-router-dom';
+
+interface AllProductsContextValues {
+  data: { products: IProduct[] };
+}
+
+const AllProductsContext = createContext<AllProductsContextValues | undefined>(
+  undefined
+);
+
+function AllProductsProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  const { data } = useLoaderData() as IProductResponse;
+  console.log(data);
+
+  return (
+    <AllProductsContext.Provider value={{ data }}>
+      {children}
+    </AllProductsContext.Provider>
+  );
+}
+
+function useAllProducts() {
+  const context = useContext(AllProductsContext);
+
+  if (context === undefined)
+    throw new Error(
+      'All Products context was used outside of AllProductsProvider'
+    );
+  return context;
+}
+
+export { AllProductsProvider, useAllProducts };
