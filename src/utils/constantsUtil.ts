f5ecaf111419319b0095bc5ebe7fc2a47b7ae651
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-24 10:52:42 +0200

    feat: SORT constat to sort filters

--- src/utils/constantsUtil.ts
+++ src/utils/constantsUtil.ts
@@ -1,2 +1,3 @@
 export const TAGS = ['motor', 'work', 'mobile', 'lifestyle'];
 export const TYPE = ['on sale', 'search'];
+export const SORT = ['oldest', 'latest', 'a-z', 'z-a', 'lowest', 'highest'];
