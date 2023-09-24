59745b857804fcbd9ab37ccbddc31506f93e5017
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-24 12:25:03 +0200

    feat: ProductContainer styles

--- client/src/assets/wrappers/ProductsContainer.ts
+++ client/src/assets/wrappers/ProductsContainer.ts
@@ -1,5 +1,29 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  margin-top: 4rem;
+
+  h2 {
+    text-transform: none;
+  }
+
+  & > h5 {
+    font-weight: 700;
+    margin-bottom: 1.5rem;
+  }
+
+  .products {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 2rem;
+  }
+
+  @media (width > 1120px) {
+    .products {
+      grid-template-columns: 1fr 1fr;
+      gap: 2rem;
+    }
+  }
+`;
 
 export default Wrapper;
