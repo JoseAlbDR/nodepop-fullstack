3cb8d34b10bd7859cbc852bf4b6a083a320c6884
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-24 02:30:48 +0200

    feat: correct tipes for getProducts service

--- client/src/services/getProducts.ts
+++ client/src/services/getProducts.ts
@@ -1,9 +1,10 @@
+import { IProductResponse } from '../types/Products';
 import customFetch from '../utils/customFetch';
 
 export const getProducts = async () => {
   try {
-    const products = await customFetch('products/');
-    return products.data;
+    const { data }: IProductResponse = await customFetch('products/');
+    return data;
   } catch (error) {
     throw new Error('Error fetching products');
   }
