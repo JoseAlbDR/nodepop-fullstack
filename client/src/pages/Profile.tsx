7acc6b30b424204254740e0663f4a3832ea9cf05
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-25 09:46:50 +0200

    feat: Profile form initial setup

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,4 +1,58 @@
+import { Form, useNavigation } from 'react-router-dom';
+import StyledProfile from '../assets/wrappers/DashboardFormPage';
+import { FormRow } from '../components';
+import { useDashboardContext } from '../context/DashboardContext';
+
 const Profile = () => {
-  return <h1>Profile</h1>;
+  const { user } = useDashboardContext();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <StyledProfile>
+      <div className="dashboard-page">
+        <Form method="post">
+          <h4>Edit {user.name} Profile</h4>
+          <div className="form-center">
+            <FormRow
+              type="text"
+              name="name"
+              labelText="name"
+              defaultValue={user.name}
+              disabled={isSubmitting}
+            />
+            <FormRow
+              type="text"
+              name="lastName"
+              labelText="last name"
+              defaultValue={user.lastName || 'demo user'}
+              disabled={isSubmitting}
+            />
+            <FormRow
+              type="email"
+              name="email"
+              labelText="email"
+              defaultValue={user.email}
+              disabled={isSubmitting}
+            />
+            <FormRow
+              type="text"
+              name="location"
+              labelText="location"
+              defaultValue={user.location || 'hot dog food truck'}
+              disabled={isSubmitting}
+            />
+            <button
+              type="submit"
+              className="btn btn-block form-btn"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? 'saving changes...' : 'save changes'}
+            </button>
+          </div>
+        </Form>
+      </div>
+    </StyledProfile>
+  );
 };
 export default Profile;
