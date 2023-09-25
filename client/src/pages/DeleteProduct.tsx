55ae6ba9147672307facd0c47631108aa8f5a0d4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-25 17:43:24 +0200

    feat: DeleteProduct page complete

--- client/src/pages/DeleteProduct.tsx
+++ client/src/pages/DeleteProduct.tsx
@@ -1,7 +1,14 @@
-import { ActionFunctionArgs, redirect } from 'react-router-dom';
+import {
+  ActionFunctionArgs,
+  Form,
+  redirect,
+  useNavigate,
+} from 'react-router-dom';
 import customFetch from '../utils/customFetch';
 import { toast } from 'react-toastify';
 import { AxiosError } from 'axios';
+import StyledDeleteProduct from '../assets/wrappers/DeleteProduct';
+import { ClickAwayListener } from '@mui/material';
 
 export const action = async (data: ActionFunctionArgs) => {
   const { params } = data;
@@ -17,3 +24,35 @@ export const action = async (data: ActionFunctionArgs) => {
     return error;
   }
 };
+
+const DeleteProduct = () => {
+  const navigate = useNavigate();
+  return (
+    <StyledDeleteProduct>
+      <Form method="post" className="confirm-form">
+        <ClickAwayListener onClickAway={() => navigate(-1)}>
+          <div className="content">
+            <header>
+              <h5>Are you sure that you want to delete the product?</h5>
+              <p>This operation cant be undone</p>
+            </header>
+            <div className="buttons">
+              <button type="submit" className="btn btn-block danger-btn">
+                Accept
+              </button>
+              <button
+                type="button"
+                className="btn btn-block cancel-btn"
+                onClick={() => navigate(-1)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </ClickAwayListener>
+      </Form>
+    </StyledDeleteProduct>
+  );
+};
+
+export default DeleteProduct;
