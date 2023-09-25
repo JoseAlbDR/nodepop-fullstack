bd5bba48f5eee27e8b5fa98497bafae3614d1ac5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-25 11:49:21 +0200

    feat: multer middleware to recieve files

---
+++ src/middleware/multerMiddleware.ts
@@ -0,0 +1,15 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, 'src/public/uploads');
+  },
+  filename: (_req, file, cb) => {
+    const fileName = Date.now() + file.originalname;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage });
+
+export default upload;
