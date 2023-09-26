4db46c8c4d5c5c3932c774e7857b90f5e57e3f70
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-26 10:08:54 +0200

    feat: user navbar menu min-width

--- client/src/assets/wrappers/Logout.ts
+++ client/src/assets/wrappers/Logout.ts
@@ -2,11 +2,13 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   position: relative;
+
   .logout-btn {
     display: flex;
     align-items: center;
     justify-content: center;
     gap: 0 0.5rem;
+    min-width: 125px;
   }
 
   .img {
