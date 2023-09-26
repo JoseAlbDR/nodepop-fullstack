a9a67a5567c0678612d321e79483065dda0adbe4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-27 01:11:45 +0200

    feat: AreaChart

--- client/src/components/AreaChart.tsx
+++ client/src/components/AreaChart.tsx
@@ -1,8 +1,26 @@
 import { IMonthlyProducts } from '../types/Products';
-
-const AreaChart = ({ data }: { data: IMonthlyProducts[] }) => {
+import {
+  ResponsiveContainer,
+  AreaChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Area,
+  Tooltip,
+} from 'recharts';
+const AreaChartComponent = ({ data }: { data: IMonthlyProducts[] }) => {
   console.log(data);
-  return <div>AreaChart</div>;
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type="monotone" dataKey="count" stroke="#8b5cf6" fill="#c4b5fd" />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 };
 
-export default AreaChart;
+export default AreaChartComponent;
