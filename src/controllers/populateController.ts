8248d53d604410f41eb8a3cedb95164ad72f6f26
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-27 01:50:11 +0200

    refactor: move al populate logic to populateService

--- src/controllers/populateController.ts
+++ src/controllers/populateController.ts
@@ -1,44 +1,14 @@
 import { Request, Response } from 'express';
-import { faker } from '@faker-js/faker';
-import { IProduct } from '../types/productInterfaces';
+
 import populateService from '../services/populateService';
 import { StatusCodes } from 'http-status-codes';
-import {
-  getRandomBoolean,
-  getRandomDateLast6Months,
-  getRandomTags,
-} from '../utils';
 
 export const populateController = {
   populateDatabase: async (req: Request, res: Response) => {
     const n = !isNaN(+req.params.n) ? +req.params.n : 10;
+    const { userId } = req.user;
 
-    const products: IProduct[] = [];
-
-    for (let i = 0; i < n; i++) {
-      const name = faker.commerce.product();
-      const image = faker.image.urlLoremFlickr({
-        width: 333,
-        height: 250,
-        category: name,
-      });
-      const price = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
-      const onSale = getRandomBoolean();
-      const tags = getRandomTags(Math.floor(Math.random() * 4) + 1);
-      const createdAt = getRandomDateLast6Months();
-      const product = {
-        name,
-        onSale,
-        price,
-        image,
-        tags,
-        createdBy: req.user.userId,
-        createdAt,
-      };
-      products.push(product);
-    }
-
-    const createdProducts = await populateService.populateDatabase(products);
+    const createdProducts = await populateService.populateDatabase(n, userId);
 
     res.status(StatusCodes.CREATED).json({ products: createdProducts });
   },
--- src/services/populateService.ts
+++ src/services/populateService.ts
@@ -1,9 +1,41 @@
+import mongoose from 'mongoose';
 import { Product } from '../models/ProductModel';
 import { IProduct } from '../types/productInterfaces';
+import {
+  getRandomBoolean,
+  getRandomDateLast6Months,
+  getRandomTags,
+} from '../utils';
+import { faker } from '@faker-js/faker';
 
 const populateService = {
-  populateDatabase: async (products: IProduct[]) => {
+  populateDatabase: async (n: number, userId: mongoose.Types.ObjectId) => {
     await Product.deleteMany();
+
+    const products: IProduct[] = [];
+
+    for (let i = 0; i < n; i++) {
+      const name = faker.commerce.product();
+      const image = faker.image.urlLoremFlickr({
+        width: 333,
+        height: 250,
+        category: name,
+      });
+      const price = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+      const onSale = getRandomBoolean();
+      const tags = getRandomTags(Math.floor(Math.random() * 4) + 1);
+      const createdAt = getRandomDateLast6Months();
+      const product = {
+        name,
+        onSale,
+        price,
+        image,
+        tags,
+        createdBy: userId,
+        createdAt,
+      };
+      products.push(product);
+    }
 
     const createdProducts = await Product.insertMany(products);
 
