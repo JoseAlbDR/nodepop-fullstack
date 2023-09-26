11fe9cdc214a47bdb1a784d54d820018cf8af9e8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-26 09:23:57 +0200

    feat: image not found error

--- src/utils/deleteImageUtil.ts
+++ src/utils/deleteImageUtil.ts
@@ -1,5 +1,6 @@
 import * as fs from 'node:fs/promises';
 import path from 'path';
+import { NotFoundError } from '../errors';
 
 export const deleteFile = async (filePath: string) => {
   try {
@@ -7,6 +8,12 @@ export const deleteFile = async (filePath: string) => {
       ? await fs.unlink(filePath)
       : await fs.unlink(path.join('src', 'public', filePath));
   } catch (error) {
+    if (
+      error instanceof Error &&
+      (error as NodeJS.ErrnoException).code === 'ENOENT'
+    ) {
+      throw new NotFoundError('Image not found');
+    }
     console.log(error);
     throw error;
   }
