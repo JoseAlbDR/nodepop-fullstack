7861e574cc0ecce2c5b70d461f3baeb3c5ec9878
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-28 00:28:26 +0200

    feat: add onChange function to IFormRowProps

--- client/src/types/FormRow.ts
+++ client/src/types/FormRow.ts
@@ -1,7 +1,10 @@
+import { SubmitFunction } from 'react-router-dom';
+
 export interface IFormRowProps {
   type: string;
   name: string;
   labelText: string;
   defaultValue: string;
   disabled?: boolean;
+  onChange: SubmitFunction;
 }
