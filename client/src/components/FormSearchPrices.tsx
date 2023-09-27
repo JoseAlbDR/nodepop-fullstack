bc04fb42ecfb78b4fb46b62c804324b5040ce93a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-28 00:28:49 +0200

    feat: price filter complete

--- client/src/components/FormSearchPrices.tsx
+++ client/src/components/FormSearchPrices.tsx
@@ -1,18 +1,18 @@
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
-import { getMinMaxPrices } from '../utils/getMinMaxPrice';
 import { useState } from 'react';
 import { useAllProductsContext } from '../context/AllProductsContext';
-const FormSearchPrices = () => {
+import { SubmitFunction } from 'react-router-dom';
+
+const FormSearchPrices = ({ onChange }: { onChange: SubmitFunction }) => {
   const { data } = useAllProductsContext();
   const [value, setValue] = useState<number[]>([0, 100000]);
+  const { minPrice, maxPrice } = data;
 
   const handleChange = (event: Event, newValue: number | number[]) => {
+    console.log('new value: ' + newValue);
     setValue(newValue as number[]);
   };
-
-  const { products } = data;
-  const { minPrice, maxPrice } = getMinMaxPrices(products);
 
   return (
     <Box
@@ -29,7 +29,21 @@ const FormSearchPrices = () => {
         max={maxPrice}
         value={value}
         step={1}
+        name="price"
         onChange={handleChange}
+        onChangeCommitted={(e, newValue) => {
+          const [min, max] = newValue as number[];
+          let price = '';
+          if (min === minPrice && max === maxPrice) {
+            price = `${minPrice}-${maxPrice}`;
+          }
+          if (min === max) price = `${min}`;
+          if (min > minPrice!) price = `${min}-`;
+          if (max < maxPrice!) price = `-${max}`;
+          if (min > minPrice! && max < maxPrice!) price = `${min}-${max}`;
+
+          onChange({ price });
+        }}
         valueLabelDisplay="auto"
         sx={{ marginLeft: 1, color: '#8b5cf6' }}
       />
