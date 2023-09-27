dfb5e3f6c9a6e0695c2a10c2ca8f29551b3ff9e6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-27 18:52:48 +0200

    refactor: change latest for newest

--- src/utils/constantsUtil.ts
+++ src/utils/constantsUtil.ts
@@ -1,3 +1,10 @@
 export const TAGS = ['motor', 'work', 'mobile', 'lifestyle'];
 export const TYPE = ['on sale', 'search'];
-export const SORT = ['oldest', 'latest', 'a-z', 'z-a', 'lowest', 'highest'];
+export const SORT = [
+  'newest',
+  'oldest',
+  'a-z',
+  'z-a',
+  'lowest price',
+  'highest price',
+];
