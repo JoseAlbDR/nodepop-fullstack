900c1b7652d52ed1dadd7bca94cffeb7e20bdd15
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-28 00:26:23 +0200

    feat: aggregation function to get max and min product prices

---
+++ src/utils/getMinMaxPrices.ts
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+import { Product } from '../models/ProductModel';
+
+export interface MinMax {
+  minPrice: number;
+  maxPrice: number;
+}
+
+export const getMinMaxPrices = async (userId?: mongoose.Types.ObjectId) => {
+  try {
+    const createdBy = userId
+      ? { createdBy: new mongoose.Types.ObjectId(userId.toString()) }
+      : {};
+    const result: MinMax[] = await Product.aggregate([
+      {
+        $match: createdBy,
+      },
+      {
+        $group: {
+          _id: null,
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $project: {
+          _id: 0, // Excluye el campo _id del resultado
+        },
+      },
+    ]);
+
+    if (result.length > 0) {
+      const { minPrice, maxPrice } = result[0];
+      return { minPrice, maxPrice };
+    } else {
+      return { minPrice: 0, maxPrice: 0 };
+    }
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
