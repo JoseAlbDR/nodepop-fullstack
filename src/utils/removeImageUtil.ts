cf441f10789f06073d96e048d33828f84d2369ae
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-30 02:47:56 +0200

    fix: debug removeImageUtil

--- src/utils/removeImageUtil.ts
+++ src/utils/removeImageUtil.ts
@@ -1,6 +1,7 @@
 import { deleteFile } from './deleteImageUtil';
 
 export const removeImage = async (image: string) => {
+  console.log('IMAGEEEEEEEEEEEE', image);
   if (image.startsWith('https') || image.endsWith('.webp')) return;
   const imagePath = image.split('/').at(-1);
   if (imagePath) await deleteFile(imagePath);
