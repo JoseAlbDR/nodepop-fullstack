6646dff28df6d6cd2b27564a81f31900266263af
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-01 00:59:51 +0200

    feat: render spinner if isLoading

--- client/src/pages/ProductsPage.tsx
+++ client/src/pages/ProductsPage.tsx
@@ -1,29 +1,23 @@
 import React from 'react';
 
 import StyledProductsPage from '../assets/wrappers/AllProductsPage';
-import { SearchContainer, ProductsContainer } from '../components';
+import { SearchContainer, ProductsContainer, Spinner } from '../components';
 
 import { ProductsProvider } from '../context/ProductsContext';
+import { useOutletContext } from 'react-router-dom';
 
 interface ProductsPageProps {
   pageType: 'user' | 'all';
 }
 
 const ProductsPage: React.FC<ProductsPageProps> = ({ pageType }) => {
+  const isLoading = useOutletContext();
+
   return (
     <StyledProductsPage>
       <ProductsProvider>
-        {pageType === 'all' ? (
-          <>
-            <SearchContainer page="all-products" />
-            <ProductsContainer />
-          </>
-        ) : (
-          <>
-            <SearchContainer page="user-products" />
-            <ProductsContainer />
-          </>
-        )}
+        <SearchContainer page={`all-${pageType}`} />
+        {isLoading ? <Spinner /> : <ProductsContainer />}
       </ProductsProvider>
     </StyledProductsPage>
   );
