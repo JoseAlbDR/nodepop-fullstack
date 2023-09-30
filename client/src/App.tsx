89711cc732066e53db06ee114899a68bcc6fb22e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-01 01:11:40 +0200

    feat: react query setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { QueryClient } from '@tanstack/query-core';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import {
   DashboardLayout,
@@ -110,6 +109,7 @@ const router = createBrowserRouter([
     ],
   },
 ]);
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
