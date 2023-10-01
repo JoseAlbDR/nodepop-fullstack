ffd91d9101e9e3677d8b1c40822eb270770f0019
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-01 16:46:41 +0200

    feat: send back and object with msg in Not found middleware

--- src/middleware/notFoundMiddleware.ts
+++ src/middleware/notFoundMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 const notFoundMiddleware = (req: Request, res: Response) => {
-  res.status(StatusCodes.NOT_FOUND).send(`Route ${req.url} not found`);
+  res.status(StatusCodes.NOT_FOUND).json({ msg: `Route ${req.url} not found` });
 };
 
 export default notFoundMiddleware;
