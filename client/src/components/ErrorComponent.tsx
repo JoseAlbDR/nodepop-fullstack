600a30685a69948eb63e9dbbd1208e113a1e01ab
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-01 16:49:11 +0200

    feat: check for AxiosError in ErrorComponent and display msg

--- client/src/components/ErrorComponent.tsx
+++ client/src/components/ErrorComponent.tsx
@@ -1,9 +1,15 @@
+import { AxiosError } from 'axios';
 import { useRouteError } from 'react-router-dom';
 
 const ErrorComponent = () => {
   const error = useRouteError();
+  if (error instanceof AxiosError) console.log(error?.response?.data.msg);
   console.log(error);
-  return <h3>There was an error...</h3>;
+  return (
+    <>
+      <h3>There was an error...</h3>
+    </>
+  );
 };
 
 export default ErrorComponent;
