1e16f36a2a00c0db8ec5d8042b9826eb6773ce7e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-02 16:30:15 +0200

    fix: removeImage route for production

--- src/utils/removeImageUtil.ts
+++ src/utils/removeImageUtil.ts
@@ -4,7 +4,7 @@ export const removeImage = async (image: string, type: string) => {
   if (!image || image.startsWith('https') || image.endsWith('.webp')) return;
   const aux = image.split('/');
   const imagePath =
-    process.env.NODE_ENV === 'render'
+    process.env.NODE_ENV === 'production'
       ? `/uploads/${type}/` + aux[aux.length - 1]
       : aux[aux.length - 1];
   if (imagePath) await deleteFile(imagePath);
