5eae6b9d5a35aed21bec14c0bcc3c099696550d0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-02 18:22:54 +0200

    feat: change tooltip styles

--- client/src/components/AreaChart.tsx
+++ client/src/components/AreaChart.tsx
@@ -9,15 +9,28 @@ import {
 } from 'recharts';
 
 import { IMonthlyProducts } from '../types/Products';
+import { useDarkThemeContext } from '../context/ToggleDarkThemeContext';
 
 const AreaChartComponent = ({ data }: { data: IMonthlyProducts[] }) => {
+  const { isDarkTheme } = useDarkThemeContext();
   return (
     <ResponsiveContainer width="100%" height={300}>
       <AreaChart data={data} margin={{ top: 50 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="date" />
         <YAxis allowDecimals={false} />
-        <Tooltip />
+        {isDarkTheme ? (
+          <Tooltip
+            contentStyle={{
+              backgroundColor: '#5b21b6',
+              color: '#fff',
+              accentColor: '#fff',
+            }}
+            itemStyle={{ color: '#fff' }}
+          />
+        ) : (
+          <Tooltip />
+        )}
         <Area type="monotone" dataKey="count" stroke="#8b5cf6" fill="#c4b5fd" />
       </AreaChart>
     </ResponsiveContainer>
--- client/src/components/BarChart.tsx
+++ client/src/components/BarChart.tsx
@@ -9,15 +9,30 @@ import {
 } from 'recharts';
 
 import { IMonthlyProducts } from '../types/Products';
+import { useDarkThemeContext } from '../context/ToggleDarkThemeContext';
 
 const BarChartComponent = ({ data }: { data: IMonthlyProducts[] }) => {
+  const { isDarkTheme } = useDarkThemeContext();
+
   return (
     <ResponsiveContainer width="100%" height={300}>
       <BarChart data={data} margin={{ top: 50 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="date" />
         <YAxis allowDecimals={false} />
-        <Tooltip />
+        {isDarkTheme ? (
+          <Tooltip
+            contentStyle={{
+              backgroundColor: '#5b21b6',
+              color: '#fff',
+              accentColor: '#fff',
+            }}
+            itemStyle={{ color: '#fff' }}
+          />
+        ) : (
+          <Tooltip />
+        )}
+
         <Bar dataKey="count" fill="#8b5cf6" barSize={75} />
       </BarChart>
     </ResponsiveContainer>
