9dfa562d70b04836b21b200fbf9a1d56971fadf1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-06 18:52:33 +0200

    feat: use middleware in likes routes

--- src/routes/likesRouter.ts
+++ src/routes/likesRouter.ts
@@ -1,9 +1,28 @@
 import express from 'express';
 import { likesController } from '../controllers/likesController';
+import {
+  validateAddLike,
+  validateIdParam,
+} from '../middleware/validationMiddleware';
+import { checkTestUser } from '../middleware/checkTestUserMiddleware';
+import { authorizePermissions } from '../middleware/authMiddleware';
 
 const router = express.Router();
 
-router.post('/', likesController.addLike);
-router.delete('/:id', likesController.deleteLike);
+router.post(
+  '/',
+  checkTestUser,
+  authorizePermissions('user', 'admin'),
+  validateAddLike,
+  likesController.addLike
+);
+
+router.delete(
+  '/:id',
+  checkTestUser,
+  authorizePermissions('user', 'admin'),
+  validateIdParam,
+  likesController.deleteLike
+);
 
 export default router;
