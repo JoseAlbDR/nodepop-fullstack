5145d6ddb314c15ae67d167f71949953c6a8d851
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-06 12:38:50 +0200

    feat: pos deleteOne hook

--- src/models/Likes.ts
+++ src/models/Likes.ts
@@ -18,15 +18,17 @@ const LikesSchema = new mongoose.Schema(
   {
     timestamps: true,
     statics: {
-      async addLikes(
+      async calculateLikes(
+        type: string,
         product: mongoose.SchemaDefinitionProperty<mongoose.Types.ObjectId>
       ) {
+        const operation = type === 'add' ? 1 : -1;
         const result: AggResult[] = await this.aggregate([
           { $match: { product } },
           {
             $group: {
               _id: null,
-              likes: { $sum: 1 },
+              likes: { $sum: operation },
             },
           },
         ]);
@@ -42,8 +44,16 @@ const LikesSchema = new mongoose.Schema(
 );
 
 LikesSchema.post('save', async function () {
-  await Likes.addLikes(this.product);
+  await Likes.calculateLikes('add', this.product);
 });
+
+LikesSchema.post(
+  'deleteOne',
+  { document: true, query: false },
+  async function () {
+    await Likes.calculateLikes('sub', this.product);
+  }
+);
 
 LikesSchema.index({ product: 1, user: 1 }, { unique: true });
 
