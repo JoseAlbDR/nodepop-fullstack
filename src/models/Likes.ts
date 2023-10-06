414c28ad79792bd16aa96fff9f127e1f597a6308
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-06 12:02:20 +0200

    feat: add static method and post save hook to add likes to a product

--- src/models/Likes.ts
+++ src/models/Likes.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+type AggResult = { _id: null; likes: number };
+
 const LikesSchema = new mongoose.Schema(
   {
     user: {
@@ -13,9 +15,36 @@ const LikesSchema = new mongoose.Schema(
       required: true,
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    statics: {
+      async addLikes(
+        product: mongoose.SchemaDefinitionProperty<mongoose.Types.ObjectId>
+      ) {
+        const result: AggResult[] = await this.aggregate([
+          { $match: { product } },
+          {
+            $group: {
+              _id: null,
+              likes: { $sum: 1 },
+            },
+          },
+        ]);
+        await mongoose.model('Product').findOneAndUpdate(
+          { _id: product },
+          {
+            likes: result[0]?.likes || 0,
+          }
+        );
+      },
+    },
+  }
 );
+
+LikesSchema.post('save', async function () {
+  await Likes.addLikes(this.product);
+});
 
 LikesSchema.index({ product: 1, user: 1 }, { unique: true });
 
-
+export const Likes = mongoose.model('Likes', LikesSchema);
