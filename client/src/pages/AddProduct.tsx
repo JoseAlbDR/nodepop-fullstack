d897da2c72b4957a6cb9fa70c12acbe03bda4253
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-08 17:56:01 +0200

    feat: handle nginx 413 error

--- client/src/pages/AddProduct.tsx
+++ client/src/pages/AddProduct.tsx
@@ -44,6 +44,8 @@ export const action =
       return redirect('all-products');
     } catch (error) {
       if (error instanceof AxiosError) {
+        if (error?.response?.status === 413)
+          toast.error('File has to be smaller than 0.5MB');
         toast.error(error?.response?.data?.msg);
       }
       console.log(error);
